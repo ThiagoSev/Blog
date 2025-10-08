00891968e7e1f74e510184b0d0134fc3a3d402c0
ry4nGP <ryan.paschoalin11@gmail.com>
2025-10-07 21:49:31 -0300

    codigo base de login concluido - frontend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyC2NR1aTaLRHTz7IDHBW7QcLup9QYpYz1E",
+    authDomain: "menteinquieta.firebaseapp.com",
+    projectId: "menteinquieta",
+    storageBucket: "menteinquieta.firebasestorage.app",
+    messagingSenderId: "631173255075",
+    appId: "1:631173255075:web:ae4d27c728e5ef0792d416"
+  };
+
+  const app = initializeApp(firebaseConfig);
+</script>
